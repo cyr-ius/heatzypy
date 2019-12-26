3e436d7212e9041727d563e93e3b0f558051ce65
cyr-ius <cyr-ius@ipocus.net>
2019-12-26 13:25:54 +0100

    Create Heatzy python API (inspired by Devotics)

---
+++ example.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+This example can be run safely as it won't change anything in your box configuration
+'''
+
+import asyncio
+import logging
+
+import heatzypy
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+username="my-login"
+password="my-password"
+
+api = heatzypy.HeatzyClient(username, password)
+
+async def async_demo():
+    devices = await api.async_get_devices()
+    for device in devices:
+        data = await api.async_get_device_info(device["did"])
+        logger.info("Heater : {} , mode : {}".format(data.get("dev_alias"),data.get("attr").get("mode")))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(async_demo())
+loop.close()
