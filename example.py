0e4ea6585386048d05d2f43f2c6d6b2836999173
cyr-ius <cyr-ius@ipocus.net>
2022-02-12 16:38:46 +0100

    Fix example

--- example.py
+++ example.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-This example can be run safely as it won't change anything in your box configuration
+This example can be run safely as it won't change anything.
 """
 
 import asyncio
@@ -21,13 +21,22 @@ logger.addHandler(ch)
 USERNAME = "my-login"
 PASSWORD = "my-password"
 
+
 async def main():
     api = HeatzyClient(USERNAME, PASSWORD)
+
     devices = await api.async_get_devices()
     for device in devices:
         name = device.get("dev_alias")
+
+        # Get data device
         data = await api.async_get_device(device["did"])
-        mode = data.get("attr").get("mode")
-        logger.info("Heater : {} , mode : {}".format(name, mode))
+        logger.info("Heater : {} , mode : {}".format(name, data.get("attr").get("mode")))
+
+        # set all Pilot v2 devices to preset 'eco' mode.
+        # await api.async_control_device(device["did"], {"attrs": {"mode": "eco"}})
+
+    await api.async_close()
+
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
