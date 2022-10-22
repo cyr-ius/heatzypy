2b2afd8378f78aea96e22346c1b43dca2c62a16b
cyr-ius <cyr-ius@ipocus.net>
2022-10-22 15:48:20 +0200

    Add exception

--- example.py
+++ example.py
@@ -8,10 +8,10 @@ This example can be run safely as it won't change anything.
 import asyncio
 import logging
 
-from heatzypy.heatzy import HeatzyClient
+from heatzypy import HeatzyClient, HeatzyException
 
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 # create console handler and set level to debug
 ch = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
@@ -24,19 +24,20 @@ PASSWORD = "my-password"
 
 async def main():
     api = HeatzyClient(USERNAME, PASSWORD)
+    try:
+        devices = await api.async_get_devices()
+        for device in devices:
+            name = device.get("dev_alias")
 
-    devices = await api.async_get_devices()
-    for device in devices:
-        name = device.get("dev_alias")
+            # Get data device
+            data = await api.async_get_device(device["did"])
+            logger.info("Heater : {} , mode : {}".format(name, data.get("attr").get("mode")))
 
-        # Get data device
-        data = await api.async_get_device(device["did"])
-        logger.info("Heater : {} , mode : {}".format(name, data.get("attr").get("mode")))
-
-        # set all Pilot v2 devices to preset 'eco' mode.
-        # await api.async_control_device(device["did"], {"attrs": {"mode": "eco"}})
-
+            # set all Pilot v2 devices to preset 'eco' mode.
+            # await api.async_control_device(device["did"], {"attrs": {"mode": "eco"}})
+    except HeatzyException as error:
+        logger.error(str(error))
     await api.async_close()
 
-loop = asyncio.get_event_loop()
+loop = asyncio.new_event_loop()
 loop.run_until_complete(main())
