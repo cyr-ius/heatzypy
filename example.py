3ff208c7a335a61f8c9ea0f5b1936f085a3cfd8e
cyr-ius <cyr-ius@ipocus.net>
2024-05-01 15:18:30 +0200

    Update example

--- example.py
+++ example.py
@@ -3,12 +3,17 @@
 
 """
 This example can be run safely as it won't change anything.
+
+There are two modes to control Heatzy modules:
+    - Classic mode by calling the Rest API
+    - Websocket mode by calling the websocket module
 """
 
 import asyncio
 import logging
+from typing import Any
 
-from heatzypy import CommandFailed, HeatzyClient, HeatzyException
+from heatzypy import HeatzyClient, HeatzyException
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -22,28 +27,39 @@ USERNAME = "my-login"
 PASSWORD = "my-password"
 
 
-async def main() -> None:
+async def async_main() -> None:
     """Main function."""
     api = HeatzyClient(USERNAME, PASSWORD)
-    try:
-        devices = await api.async_get_devices()
+
+    def callback(devices: dict[str, Any]) -> None:
+        """Display devices."""
         for uniqe_id, device in devices.items():
             name = device.get("dev_alias")
+            mode = device.get("attrs").get("mode")
+            lock = device.get("attrs").get("lock_switch")
+            logger.info("Heater: %s ,mode: %s,lock: %s", name, mode, lock)
+        logger.info("---------------------------------------")
 
-            # Get data device
-            data = await api.async_get_device(uniqe_id)
-            mode = data.get("attr", {}).get("mode")
-            logger.info("Heater : %s , mode : %s", name, mode)
+    # Call Heatzy Rest API
 
+    try:
+        devices = await api.async_get_devices()
+        callback(devices)
+        for uniqe_id, device in devices.items():
             # set all Pilot v2 devices to preset 'eco' mode.
             try:
-                await api.async_control_device(uniqe_id, {"attrs": {"mode": "stop"}})
-            except CommandFailed as error:
+                # await api.async_control_device(uniqe_id, {"attrs": {"mode": "eco"}})
+                pass
+            except HeatzyException as error:
                 logger.error(error)
     except HeatzyException as error:
         logger.error(str(error))
-    await api.async_close()
+
+    # Listen Heatzy webscoket
+
+    await api.websocket.async_listen(callback=callback, all_devices=True)
 
 
-loop = asyncio.new_event_loop()
-loop.run_until_complete(main())
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(async_main())
