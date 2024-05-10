388720194ddf5f7a04585a33011d7694584de313
cyr-ius <cyr-ius@ipocus.net>
2024-05-10 12:34:15 +0200

    Add tests

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -2,22 +2,61 @@
 
 from __future__ import annotations
 
-from unittest.mock import patch
+import asyncio
+from unittest.mock import AsyncMock, patch
 
+from aiohttp import ClientError, ClientResponseError
 import pytest
 
-from heatzypy import HeatzyClient
+from heatzypy import AuthenticationFailed, HeatzyClient, TimeoutExceededError
+from heatzypy.exception import HttpRequestFailed
 from tests import mock_response
 
 
 @pytest.mark.asyncio
-async def test_connect(api, mock_token) -> None:
-    """Test connection."""
-    mock = mock_response(mock_token)
-    with patch("aiohttp.ClientSession.request", return_value=mock()):
-        await api._auth.async_get_token()
+async def test_init() -> None:
+    """Init api."""
+    api = HeatzyClient("x", "y")
+    assert api.websocket.is_connected is False
+    assert api._auth._username == "x"
 
+
+@pytest.mark.asyncio
+async def test_request(api, req_token, mock_devices) -> None:
+    """Test connection."""
+    mock = mock_response(mock_devices)
+    with patch("aiohttp.ClientSession.request", side_effect=[req_token(), mock()]):
+        await api.async_bindings()
+
+    assert len(req_token.mock_calls) == 3
     assert len(mock.mock_calls) == 3
+
+
+@pytest.mark.asyncio
+async def test_error_request(api) -> None:
+    """Test connection."""
+    with patch("aiohttp.ClientSession.request", side_effect=[asyncio.TimeoutError]):
+        try:
+            await api.async_bindings()
+        except TimeoutExceededError as error:
+            assert error is not None
+
+    with patch("aiohttp.ClientSession.request", side_effect=[ClientError]):
+        try:
+            await api.async_bindings()
+        except HttpRequestFailed as error:
+            assert error is not None
+
+    with patch(
+        "aiohttp.ClientSession.request",
+        side_effect=[
+            ClientResponseError(request_info=AsyncMock(), history=(AsyncMock()))
+        ],
+    ):
+        try:
+            await api.async_bindings()
+        except AuthenticationFailed as error:
+            assert error is not None
 
 
 @pytest.mark.asyncio
@@ -49,7 +88,7 @@ async def test_get_devices(api, mock_token, mock_devices, mock_attribut) -> None
     ):
         devices = await api.async_get_devices()
 
-    assert devices is not None
+    assert devices.get("ZtTGUB8Li86z7TG9A7XTQY") is not None
 
 
 @pytest.mark.asyncio
@@ -71,21 +110,13 @@ async def test_get_device(api, mock_token, mock_device, mock_attribut) -> None:
 
 
 @pytest.mark.asyncio
+@patch("heatzypy.auth.Auth.async_get_token")
 async def test_control(api, mock_token) -> None:
     """test send control."""
     mock = mock_response()
     with (
-        patch("heatzypy.auth.Auth.async_get_token", return_value=mock_token),
         patch("aiohttp.ClientSession.request", return_value=mock()),
     ):
         await api.async_control_device("ZtTGUB8Li86z7TG9A7XTQY", {"mode": "cft"})
 
-    assert len(mock.mock_calls) == 3
-
-
-@pytest.mark.asyncio
-async def test_init() -> None:
-    """Init api."""
-    api = HeatzyClient("x", "y")
-    assert api.websocket.is_connected is False
-    assert api._auth._username == "x"
+    assert len(mock.mock_calls) == 1
